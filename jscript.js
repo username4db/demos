25bb07e912251e382657970799cd4d76552e4660
username4db <50797481+username4db@users.noreply.github.com>
2020-07-05 09:42:54 +0800

    Create jscript.js

---
+++ jscript.js
@@ -0,0 +1,10 @@
+var jj = 3;
+
+//console.log(jj);
+
+import { getUsefulContents, get3 } from './modules/module.js';
+
+
+getUsefulContents('index.html', function (data) {
+	//console.log(data);
+});
